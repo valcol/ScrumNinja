214f46ef84e5f2aa37fc0e82d973c1fe1201c34c
valcol <valcol.nc@gmail.com>
2016-11-02 18:40:21 +0100

    refactor(projects): move project related functions out of main js file

--- app/imports/api/server/main.js
+++ app/imports/api/server/main.js
@@ -1,61 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import { Collections } from '../collections.js';
-import { check, Match } from 'meteor/check';
-import moment from 'moment';
-
-function createProject(project) {
-  check(project, {
-    name: String,
-    start: String,
-    end: String,
-    visibility: Match.OneOf('public', 'private'),
-    description: String
-  });
-
-  if(!moment(project.start).isValid() || !moment(project.end).isValid())
-    throw new Meteor.Error('date format unsupported');
-
-  if(moment(project.start).isAfter(project.end))
-      throw new Meteor.Error('start date must be before end date');
-
-  let regex = /^[a-zA-Z ]{2,30}$/;
-  if(!regex.test(project.name))
-    throw new Meteor.Error('project name must only contains between 2 and 30 letters, without space');
-
-  if(Collections.Projects.findOne({name: project.name}))
-    throw new Meteor.Error('name already taken');
-
-  // Make sure the user is logged in before inserting a project
-  /*if (! this.userId) {
-    throw new Meteor.Error('not-authorized');
-  }*/
-
-  Collections.Projects.insert({
-    name: project.name,
-    start: moment(project.start).toDate(),
-    end: moment(project.end).toDate(),
-    visibility: project.visibility,
-    description: project.description
-  });
-
-   return 'Project created!';
-}
-
-function deleteProject(name){
-  check(name, String);
-
-  Collections.Projects.remove({
-    name
-  });
-
-   return 'Project deleted';
-}
+import Projects from './projects';
 
 Meteor.startup(() => {
   // code to run on server at startup
 });
 
 Meteor.methods({
-  'project.create': createProject,
-  'project.delete': deleteProject
+  'project.create': Projects.create,
+  'project.delete': Projects.delete
 });
---
+++ app/imports/api/server/projects/index.js
@@ -0,0 +1,51 @@
+import { Meteor } from 'meteor/meteor';
+import { Collections } from '../../collections.js';
+import { check, Match } from 'meteor/check';
+import moment from 'moment';
+
+let Projects = function() {};
+
+Projects.prototype.create = function(project) {
+  check(project, {
+    name: String,
+    start: String,
+    end: String,
+    visibility: Match.OneOf('public', 'private'),
+    description: String
+  });
+
+  if(!moment(project.start).isValid() || !moment(project.end).isValid())
+    throw new Meteor.Error('date format unsupported');
+
+  if(moment(project.start).isAfter(project.end))
+      throw new Meteor.Error('start date must be before end date');
+
+  let regex = /^[a-zA-Z ]{2,30}$/;
+  if(!regex.test(project.name))
+    throw new Meteor.Error('project name must only contains between 2 and 30 letters, without space');
+
+  if(Collections.Projects.findOne({name: project.name}))
+    throw new Meteor.Error('name already taken');
+
+  Collections.Projects.insert({
+    name: project.name,
+    start: moment(project.start).toDate(),
+    end: moment(project.end).toDate(),
+    visibility: project.visibility,
+    description: project.description
+  });
+
+   return 'Project created';
+};
+
+Projects.prototype.delete = function(name){
+  check(name, String);
+
+  Collections.Projects.remove({
+    name
+  });
+
+   return 'Project deleted';
+};
+
+export default new Projects();
