d7bd715fd66a8f9d00c6cea77b534f4bcc3102b5
qramas <quentinusradus@hotmail.fr>
2016-11-03 19:26:53 +0100

    feat(uploadCdc): add 1st version of upload CDC

--- app/imports/api/collections.js
+++ app/imports/api/collections.js
@@ -8,4 +8,28 @@ Meteor.isClient && Template.registerHelper('Collections', CollectionsObj);
 // Add collections here
 CollectionsObj.Projects = new Mongo.Collection('projects');
 
+this.CDC = new Meteor.Files({
+  debug: true,
+  collectionName: 'CDC',
+  allowClientCode: true, // Disallow remove files from Client
+  onBeforeUpload: function (file) {
+    // Allow upload files under 10MB, and only in pdf formats
+    if (file.size <= 1024*1024*10 && /pdf/i.test(file.extension)) {
+      return true;
+    } else {
+      return 'Please upload pdf, with size equal or less than 10MB';
+    }
+  }
+});
+
+if (Meteor.isServer) {
+  CDC.denyClient();
+  Meteor.publish('files.CDC.all', function () {
+    return CDC.find().cursor;
+  });
+
+} else {
+  Meteor.subscribe('files.CDC.all');
+}
+
 export const Collections = CollectionsObj;
