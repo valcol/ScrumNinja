39e6c1c8ddcde9daa4693e809e92b93ae74bcda5
valcol <valcol.nc@gmail.com>
2016-11-04 16:24:52 +0100

    tests(acceptance): fix previous strange bug

--- app/tests/acceptance/step_definitions/When_I_put_signupfields.js
+++ app/tests/acceptance/step_definitions/When_I_put_signupfields.js
@@ -1,17 +1,10 @@
 module.exports = function() {
-  this.When(/^I put sign up fields$/, function (arg) {
+  this.When(/^I put sign up fields$/, function () {
     browser.waitForExist('.register-box');
-    browser.setValue('input[name="fullname"]', "Lwi");
-    browser.setValue('input[name="email"]', "llipwig10@laposte.net");
+    browser.setValue('input[name="fullname"]', "Lwi41");
+    browser.setValue('input[name="email"]', "llipwi41g10@laposte.net");
     browser.setValue('input[name="password"]', "leo");
     browser.setValue('input[name="repassword"]', "leo");
-    /*browser.setValue('input[name="fullname"]', "Léon Lipwig");
-    browser.setValue('input[name="email"]', "llipwig@laposte.net");*/
     browser.click('.register-box .btn');
-
-    //browser.waitUntil(function(){ return browser.isExisting('.content-header');},5000);
-    //browser.waitForExist('.content-header', 5000);
-    //expect('My Projects').toEqual(browser.getText('.content-header h1'));
-    expect(true);
   });
 };
