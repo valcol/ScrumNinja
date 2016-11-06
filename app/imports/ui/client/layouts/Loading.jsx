f2bc45eabff7a7e7745229d94e9635cac80e380e
valcol <valcol.nc@gmail.com>
2016-11-06 02:29:01 +0100

    feat(all): add loading component

---
+++ app/imports/ui/client/layouts/Loading.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Loading = () =>         <div className="overlay">
+                                        <i className="fa fa-refresh fa-spin" />
+                                      </div>;
